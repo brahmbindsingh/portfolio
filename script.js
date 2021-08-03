3cdb1263d3236063e9fc209b4e27c30539848451
Brahm Bind Singh <singhbrahmbind@gmail.com>
2021-08-03 11:06:21 +0530

    some changes

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ t1.to('.slider',{y: "-100%",duration:1.5,delay:.5});
 t1.to('.intro',{y:"-100%",duration:1},"-=1");
 t1.fromTo('nav',{opacity:0},{opacity:1,duration:1});
 t1.fromTo('.job-names',{opacity:0},{opacity:1,duration:1});
+
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 300) {
+//           $(".black").css("background" , "blue");
+//         }
+  
+//         else{
+//             $(".black").css("background" , "#333");  	
+//         }
+//     })
+//   })
